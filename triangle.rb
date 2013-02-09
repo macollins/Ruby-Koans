e17d7a0e11d296d931250ad8a5e8c335372bf7db
Michael Collins <macollins88@gmail.com>
2013-02-09 13:03:55 -0800

    Completed second triangle koan, iteration and blocks

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  sides = [a, b, c]
+  raise TriangleError unless valid? sides
+
   if equilateral?(a, b, c)
     :equilateral
   elsif isosceles?(a, b, c)
@@ -29,6 +33,20 @@ end
 
 def isosceles?(a, b, c)
   a == b || b == c || a == c
+end
+
+def valid?(sides)
+  positive_length_sides?(sides) && sum_of_two_sides_exceed_third_side?(sides)
+end
+
+def positive_length_sides?(sides)
+  sides.all? { |side| side >= 0 }
+end
+
+# Sum of any two sides of a triangle always exceed the length of the third side.
+def sum_of_two_sides_exceed_third_side?(sides)
+  sorted_sides = sides.sort
+  sorted_sides[0] + sorted_sides[1] > sorted_sides[2]
 end
 
 # Error class used in part 2.  No need to change this code.
